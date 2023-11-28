ebc84055c7ff6df64a6d2db98ba5adaf011528ef
Camila Souza <eusoumilasouza@gmail.com>
2023-11-28 16:32:07 +0000

    Atividade

---
+++ app/src/main/java/application/model/AlternativaRepository.java
@@ -0,0 +1,8 @@
+package application.model;
+
+import org.springframework.data.repository.CrudRepository;
+import application.model.Alternativa;
+
+public interface AlternativaRepository  extends CrudRepository<Alternativa, Integer> {
+    
+}
---
+++ app/src/main/java/application/model/QuestaoRepository.java
@@ -0,0 +1,9 @@
+package application.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface QuestaoRepository extends CrudRepository<Questao, Integer> {
+    
+    
+}
